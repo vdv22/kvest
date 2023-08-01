81c3030e430f2afd27c5d156e02372180dff82a8
Alex <vdv2@mail.ru>
2023-08-01 22:57:20 +0700

    add

--- number_new/main.c
+++ number_new/main.c
@@ -5,50 +5,51 @@ int main(void)
 {
    
    DDRB|=(1<<4)|(1<<3)|(1<<2)|(1<<1)|(1<<0);
-   DDRD|=(1<<7)|(1<<6)|(1<<5)|(1<<2);
-   DDRD|= (1<<6);
-   
+   DDRD|=(1<<7)|(1<<6)|(1<<5)|(1<<4);
+   DDRC|= (1<<3);                                // управление реле   
+   DDRC&=~(1<<2);                                // кнопка
+   PORTC|=(1<<2);
 while (1) 
 {
 	      
 	
-		if ((PIND&(1<<2))==0)
+		if ((PINC&(1<<2))==0)
 		{
 			ten;
 			_delay_ms(1000);
-			if((PIND&(1<<2))!=0)
+			if((PINC&(1<<2))!=0)
 			continue;
 			
 			dark;
 			nine;
 			_delay_ms(1000);
-			if((PIND&(1<<2))!=0)
+			if((PINC&(1<<2))!=0)
 			continue;
 			
 			dark;
 			eigth;
 			_delay_ms(1000);
 			
-			if((PIND&(1<<2))!=0)
+			if((PINC&(1<<2))!=0)
 			continue;
 			
 			dark;
 			seven;
 			_delay_ms(1000);
 			
-			if((PIND&(1<<2))!=0)
+			if((PINC&(1<<2))!=0)
 			continue;
 			
 			dark;
 			six;
 			_delay_ms(1000);
-			if((PIND&(1<<2))!=0)
+			if((PINC&(1<<2))!=0)
 			continue;
 			
 			dark;
 			five;			
 			_delay_ms(1000);
-			if((PIND&(1<<2))!=0)
+			if((PINC&(1<<2))!=0)
 			continue;
 			
 			dark;
@@ -56,27 +57,27 @@ while (1)
 			_delay_ms(1000);			
 			
 			_delay_ms(1000);
-			if((PIND&(1<<2))!=0)			
+			if((PINC&(1<<2))!=0)			
 			continue;
 			
 			dark;
 			three;
 			_delay_ms(1000);
 			
-			if((PIND&(1<<2))!=0)
+			if((PINC&(1<<2))!=0)
 			continue;
 			
 			dark;
 			two;
 			_delay_ms(1000);			
 			
-			if((PIND&(1<<2))!=0)
+			if((PINC&(1<<2))!=0)
 			continue;
 			
 			dark;
 			one;
 			_delay_ms(1000);
-			if((PIND&(1<<2))!=0)
+			if((PINC&(1<<2))!=0)
 			continue;
 			
 			dark;
@@ -84,9 +85,8 @@ while (1)
 			_delay_ms(1000);
 			
 			
-									
 			
-		}
+			}
 		
 }
 
